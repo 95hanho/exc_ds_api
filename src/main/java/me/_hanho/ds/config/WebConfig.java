95849f62e2cd85b5130adcd6e5b051ef01407197
95hanho <hosung6674@naver.com>
2025-06-23 16:55:20 +0900

    123

--- src/main/java/me/_hanho/ds/config/WebConfig.java
+++ src/main/java/me/_hanho/ds/config/WebConfig.java
@@ -17,8 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 //			.allowedOrigins("*");
 			.allowedOrigins("http://localhost:5173/", "http://localhost:5174/", "https://95hanho.pe.kr/") // 클라이언트 도메인 명시
-		 	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드 명시
-	        .allowedHeaders("*") // 허용할 헤더
+			.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드 명시
+//		 	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드 명시
+//	        .allowedHeaders("*") // 허용할 헤더
 	        .allowCredentials(true) // 인증 정보(쿠키) 허용 여부
 	        .maxAge(3600); // preflight 요청 캐싱 시간 (초 단위)
 	}
